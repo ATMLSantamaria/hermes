c008ec9be31d21a833a72a3c57013ecd12ce88ee
ATMLSantamaria <alejandroprivado8@gmail.com>
2020-04-19 18:15:04 +0200

    Mejora en aplicacion funcional

---
+++ Automatic_Data/Programa_limpio.py
@@ -0,0 +1,90 @@
+import urllib.request, urllib.parse, urllib.error
+import ssl
+import json
+import requests
+import sqlite3
+import threading, time
+def Introduce_In_DB(received_data,db_name):
+    conn = sqlite3.connect(db_name)
+    cur = conn.cursor()
+
+    cur.executescript('''
+    DROP TABLE IF EXISTS STOCKS2;
+    CREATE TABLE IF NOT EXISTS STOCKS_TODAY (
+        id   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        symbol TEXT UNIQUE,
+        companyName TEXT,
+        latestPrice TEXT,
+        peRATIO TEXT
+        )
+    ''')
+    print(d['symbol'])
+    print(d['companyName'])
+    print(d['latestPrice'])
+    print(d['peRatio'])
+    cur.execute('''INSERT OR IGNORE INTO STOCKS_TODAY (symbol, companyName, latestPrice, peRatio)
+                VALUES (?, ?, ?, ? )''', (d['symbol'],d['companyName'],d['latestPrice'],d['peRatio'] ) )
+
+    conn.commit()
+
+    return 0
+
+def Descargar_Datos(serviceurl,version,caracteristica,symbol,tipo,CLAVE):
+    url_local=serviceurl+version+caracteristica+symbol+tipo+'?token='+CLAVE
+    print(url_local)
+    data=requests.get(url_local).json()
+    url_dividens=serviceurl+version+caracteristica+symbol+'/dividends/5y'+'?token='+CLAVE
+    #ESTE 5y implica que pedimos los dividendos de hace 5 años
+    #Podemos poner 4y, 3y,2y,1y,ytd (este año), next (el proximo)
+    data_dividends=requests.get(url_dividens).json()
+    print("LOS DATOS DE DIVIDENDOS DE", symbol)
+    try:
+        lista_claves=data_dividends[0].keys()
+        print("lista claves",lista_claves)
+        for i in lista_claves:
+            print("i es", i)
+            print(i," = ",data_dividends[0][i])
+            print("-------------")
+        return data
+    except:
+        print("No hay datos de dividendos")
+        return data
+
+
+election = input("elija modo funcional(f) o de prueba(p)")
+if (election=='f'):
+    CLAVE = 
+    serviceurl = 
+    database= 'real_stocks_db.sqlite'
+elif(election=='p'):
+    CLAVE = 'Tpk_385aaf6516544f10bbd63f3ff5f87b4b'
+    serviceurl = 'https://sandbox.iexapis.com'
+    database= 'prueba_stocks_db.sqlite'
+else:
+    CLAVE = 'Tpk_385aaf6516544f10bbd63f3ff5f87b4b'
+    serviceurl = 'https://sandbox.iexapis.com'
+    database= 'prueba_stocks_db.sqlite'
+
+
+
+
+
+version = '/stable'
+#/stock/{symbol}/dividends/{range}
+caracteristica= '/stock' #
+#simbol= '/xom'
+tipo = '/quote' # /company
+list_stocks=['/xom','/vod','/upwk','/acs']
+connn = sqlite3.connect(database)
+curr = connn.cursor()
+curr.executescript('''DROP TABLE IF EXISTS STOCKS_TODAY''')
+curr.executescript('''DROP TABLE IF EXISTS STOCKS2''')
+connn.commit()
+
+
+for k in list_stocks:
+    print(k)
+    d=Descargar_Datos(serviceurl,version,caracteristica,k,tipo,CLAVE)
+    #print(d['symbol'])
+    #print(d)
+    d=Introduce_In_DB(d,database)
--- Automatic_Data/prueba.py
+++ Automatic_Data/prueba.py
@@ -3,9 +3,10 @@ import ssl
 import json
 import requests
 import sqlite3
-
+import threading, time
 #curl -k 'https://sandbox.iexapis.com/stable/stock/aapl/quote?token=YOUR_TOKEN_HERE'
 # -> para usar con bash -> curl -k 'https://cloud.iexapis.com/stable/stock/aapl/quote?token=YOUR_TOKEN_HERE'
+
 #NECESARIO PARA ENTRAR A LA API DE GOOGLE CON MI CUENTA
 CLAVE = 'Tpk_385aaf6516544f10bbd63f3ff5f87b4b'
 #serviceurl = 'https://cloud.iexapis.com/'
@@ -23,76 +24,92 @@ version = '/stable'
 caracteristica= '/stock' #
 simbol= '/xom'
 tipo = '/quote' # /company
-#/company te da otro tipo de datos
 
-url=serviceurl+version+caracteristica+simbol+'/quote?token='+CLAVE
-url2=serviceurl+version+caracteristica+simbol+'/company?token='+CLAVE
+#/company te da otro tipo de datos
+#url=serviceurl+version+caracteristica+simbol+'/quote?token='+CLAVE
+#url2=serviceurl+version+caracteristica+simbol+'/company?token='+CLAVE
 #url3=url=serviceurl+version+caracteristica+simbol+'/quote/companyName?token='+CLAVE
-print('https://sandbox.iexapis.com/stable/stock/aapl/quote?token=Tpk_385aaf6516544f10bbd63f3ff5f87b4b')
-print(url)
+#print('https://sandbox.iexapis.com/stable/stock/aapl/quote?token=Tpk_385aaf6516544f10bbd63f3ff5f87b4b')
+#print(url)
 
 #datos=raw_datos.json()
 print("---------------")
-datos1 = requests.get(url).json()
-datos2 = requests.get(url2).json()
+#datos1 = requests.get(url).json()
+#datos2 = requests.get(url2).json()
+def Descargar_Datos(serviceurl,version,caracteristica,symbol,tipo,CLAVE):
+    #threading.Timer(5.0, Descargar_Datos.start()
+    url_local=serviceurl+version+caracteristica+symbol+tipo+'?token='+CLAVE
+    print(url_local)
+    data=requests.get(url_local).json()
+    url_local='hsjsjh'
+    return data
 #datos3 = requests.get(url3).json()
 #request3 = requests.get('https://sandbox.iexapis.com/stable/stock/aapl/quote?token=Tpk_385aaf6516544f10bbd63f3ff5f87b4b').json()
-print("datos",datos1)
-print("---------------")
-print(datos2)
-print("-----------")
+#print("datos",datos1)
+#print("---------------")
+#print(datos2)
+#print("-----------")
 #print(datos3)
 
 #datos es un diccionatio
-print("--------")
-lista_claves=datos1.keys()
-for i in lista_claves:
-    print (i,"=",datos1[i]) 
+#print("--------")
+#lista_claves=datos1.keys()
+#for i in lista_claves:
+#    print (i,"=",datos1[i]) 
 #request2 = urllib.request.get(url)
-#EL PROGRAMA SE CONECTA Y RECIBE UN STRING
-##print('RETRIEVING',url)
-#print('hasta aqui funciona')
-#fhand=urllib.request.urlopen(url)
-#data=fhand.read().decode()
-#data=urllib.request.urlopen(url,context=ctx).read().decode()
-#print('RETRIEVED',len(data),'characters')
+
 #EL PROGRAMA HA RECIBIDO EL STRING Y LO IMPRIMO para
 #ENTENDERLO
-print('------------------')
+#print('------------------')
 #print(data)
 vector_relevant_keys=['symbol','companyName','latestPrice','peRatio',]
-for element in vector_relevant_keys:
-    print(element,"=",datos1[element])
+#for element in vector_relevant_keys:
+#    print(element,"=",datos1[element])
 
-print('------------------')
-#json.loads(data) lee json y lo convierte en un diccionario-lista
-#js=json.loads(data)
-#print(js)
-#ahora unicamente tengo que acceder a lo que yo QUIERO
-#pst_code=js['results'][0]['address_components'][5]['long_name']
-#print('postal_code',pst_code)
+#print('------------------hfhfhfhfh')
 
 
-conn = sqlite3.connect('stocks.sqlite')
-cur = conn.cursor()
 
-cur.executescript('''
-CREATE TABLE IF NOT EXISTS STOCKS_TODAY (
-    id   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    symbol TEXT UNIQUE,
-    companyName TEXT,
-    latestPrice TEXT,
-    peRATIO TEXT
-    )
-''')
-cur.execute('''
-CREATE TABLE IF NOT EXISTS STOCKS2 (SYMBOL TEXT, COMPANY_NAME TEXT, latestPrice REAL, peRATIO REAL)''')
-print(datos1['symbol'])
-print(datos1['companyName'])
-print(datos1['latestPrice'])
-print(datos1['peRatio'])
-cur.execute('''INSERT INTO STOCKS_TODAY (symbol, companyName, latestPrice, peRatio)
-            VALUES (?, ?, ?, ? )''', (datos1['symbol'],datos1['companyName'],datos1['latestPrice'],datos1['peRatio'] ) )
 
-conn.commit()
+#EL PROBLEMA CON ESTA BASE DE DATOS ES QUE NO SE VA A ACTUALIZAR
+def Introduce_In_DB(d):
+    conn = sqlite3.connect('stocks.sqlite')
+    cur = conn.cursor()
+
+    cur.executescript('''
+    DROP TABLE IF EXISTS STOCKS2;
+    CREATE TABLE IF NOT EXISTS STOCKS_TODAY (
+        id   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        symbol TEXT UNIQUE,
+        companyName TEXT,
+        latestPrice TEXT,
+        peRATIO TEXT
+        )
+    ''')
+    #cur.execute('''
+    #CREATE TABLE IF NOT EXISTS STOCKS2 (SYMBOL TEXT, COMPANY_NAME TEXT, latestPrice REAL, peRATIO REAL)''')
+    print(d['symbol'])
+    print(d['companyName'])
+    print(d['latestPrice'])
+    print(d['peRatio'])
+    cur.execute('''INSERT OR IGNORE INTO STOCKS_TODAY (symbol, companyName, latestPrice, peRatio)
+                VALUES (?, ?, ?, ? )''', (d['symbol'],d['companyName'],d['latestPrice'],d['peRatio'] ) )
+
+    conn.commit()
+
+    return 0
+connn = sqlite3.connect('stocks.sqlite')
+curr = connn.cursor()
+curr.executescript('''DROP TABLE IF EXISTS STOCKS_TODAY''')
+connn.commit()
+list_stocks=['/xom','/vod','/upwk','']
+
+for k in list_stocks:
+    print(k)
+    d=Descargar_Datos(serviceurl,version,caracteristica,k,tipo,CLAVE)
+    #print(d['symbol'])
+    #print(d)
+    #print("estamos en el bucle")
+    Introduce_In_DB(d)
+
 
